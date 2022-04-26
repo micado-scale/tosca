88f4c655a5b1e7eb1c229654a8924e0e79e6db7a
jaydesl <jay.deslauriers@gmail.com>
2022-04-26 10:32:29 +0000

    feat(ci): autogen flat typedef tar

---
+++ flatpack-script.sh
@@ -0,0 +1,25 @@
+echo "Flattening directory structure and formatting imports..."
+
+format_imports() {
+	yq -i '.imports[] |= sub(".*custom_types/", "")' $1
+	yq -i '.imports[] |= sub("/", "_")' $1
+}
+tmpdir=$PWD/flatpack_tmp
+export -f format_imports
+export tmpdir
+
+mkdir $tmpdir
+
+cd custom_types
+find */ -type f -exec bash -c 'file=${1#./}; cp "$file" "$tmpdir/${file//\//_}"' _ '{}' \;
+cd ..
+
+cp micado_types.yaml $tmpdir
+
+find $tmpdir -type f -exec bash -c 'format_imports "$@"' _ '{}' \;
+
+tar -C $tmpdir -cf micado_types.tar ./
+
+rm -r $tmpdir
+
+echo "DONE"
